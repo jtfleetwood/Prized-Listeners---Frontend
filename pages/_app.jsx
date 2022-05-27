3e934d7a2bd033f107b21c714a5f7aad9505fcfa
JT <jfleetw@clemson.edu>
2022-05-27 17:55:11 -0400

    Removed unnecessary Auth0 SDK components (API Server, Docker, and related configuration files)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
-
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import initFontAwesome from '../utils/initFontAwesome';
 import '../styles/globals.css';
-import './assets/css/style.css'
 import 'bootstrap/dist/css/bootstrap.css'
-initFontAwesome();
 
 export default function App({ Component, pageProps }) {
   return (
